e5375c2a8304f15bafcea213f03f4cd5c4a6a443
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-10-13 16:28:20 -0600

    Finished the backend section

--- app.py
+++ app.py
@@ -6,6 +6,33 @@ import json
 
 app = Flask(__name__)
 
+
+@app.post('/api/pokemon')
+def post_pokemon():
+    is_valid = check_endpoint_info(
+        request.json, ['name', 'description', 'image_url'])
+    if (is_valid != None):
+        return make_response(json.dumps(is_valid, default=str), 400)
+
+    results = run_statement('CALL insert_pokemon(?,?,?)', [request.json.get(
+        'name'), request.json.get('description'), request.json.get('image_url')])
+
+    if (type(results) == list):
+        return make_response(json.dumps(results[0][0], default=str), 200)
+    else:
+        return make_response(json.dumps("Sorry, an error has occured.", default=str), 500)
+
+
+@app.get('/api/pokemon')
+def get_all_pokemons():
+    results = run_statement('CALL get_all_pokemons()')
+
+    if (type(results) == list):
+        return make_response(json.dumps(results, default=str), 200)
+    else:
+        return make_response(json.dumps("Sorry, an error has occurred.", default=str), 500)
+
+
 if (production_mode):
     print("Running in Production Mode")
     app.run(debug=True)
